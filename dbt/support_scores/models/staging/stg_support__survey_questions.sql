76314b310e4eeb26b2d23f1b86b64805b17248fb
Alec MacMillen <alecmacmillen@gmail.com>
2026-08-12 23:16:30 -0600

    first model created, file structure cleaned up

---
+++ dbt/support_scores/models/staging/stg_support__survey_questions.sql
@@ -0,0 +1,8 @@
+SELECT survey_question_id
+    ,survey_question_type
+    ,survey_question_name
+    ,survey_question_text
+FROM abstrat.support.survey_questions
+WHERE survey_question_id IS NOT NULL
+    AND state_code = 'GA'
+    AND survey_question_cycle = '2022'
