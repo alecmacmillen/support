bc618a733c6b4dca2f8f59fc5d16f10b9a963cdd
Alec MacMillen <alecmacmillen@gmail.com>
2026-08-24 18:08:04 -0600

    create binomial support var in survey responses

--- dbt/support/models/staging/stg_support__contacts_survey_responses.sql
+++ dbt/support/models/staging/stg_support__contacts_survey_responses.sql
@@ -4,5 +4,9 @@ SELECT contacts_survey_response_id
     ,survey_question_id
     ,survey_response_id
     ,datetime_canvassed
+    ,CASE 
+        WHEN survey_response_id IN ('2102006','2114703','2102007','2114704') THEN 1
+        WHEN survey_response_id IN ('2102009','2114706','2102010','2114710','2114700','2114712') THEN 0
+        ELSE NULL END AS binomial_support
 FROM abstrat.support.contacts_survey_responses_myv
 WHERE survey_question_id IN ('514051','517214')
